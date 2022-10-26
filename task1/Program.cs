617463e7549cc4df083a4ec02ab3318593022971
Luba <bart6891@ya.ru>
2022-10-26 16:38:18 +0300

    real numbers in a two-dimensional array

---
+++ task1/Program.cs
@@ -0,0 +1,32 @@
+﻿int rows = ReadInt("количество строк m =  ");
+int colums = ReadInt("количество столбцов n =  ");
+double[,] numbers = new double[rows, colums];
+FillArray(numbers);
+PrintArray(numbers);
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100) / 10.0;
+        }
+    }
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
